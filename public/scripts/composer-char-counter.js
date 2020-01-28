1a9b99c7a6605e2448200ed306332d9149127701
CLARE NOLAN <clarenolan92@gmail.com>
2020-01-28 23:41:03 +0000

    tweet container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
   let maxLength = 140;
   $("textarea").keypress(function() {
     let textLength =  maxLength - $(this).val().length;
-    // console.log(remainingLength);
     $('.counter').text(textLength);
-    $(".counter").toggleClass("counter-red", textLength < 0);
+    $(".counter").toggleClass("counter-red", textLength < 0); //change to red class when the count is negative
   });
 });
 
