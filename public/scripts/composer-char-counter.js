40cf2495e38c3e5b8f326028457bcc00debb3261
CLARE NOLAN <clarenolan92@gmail.com>
2020-01-28 22:02:14 +0000

    negative character count is red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     let textLength =  maxLength - $(this).val().length;
     // console.log(remainingLength);
     $('.counter').text(textLength);
+    $("span:contains('-')").css("color", "#f00");
   });
 });
 
