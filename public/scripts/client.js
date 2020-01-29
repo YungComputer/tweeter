5a98d50594fe70d4f4b7ef8489f475a421b79d0a
CLARE NOLAN <clarenolan92@gmail.com>
2020-01-29 04:14:21 +0000

    render tweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,38 +5,58 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac"
-  },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants"
-  },
-  created_at: 1461116232227
-};
+$(document).ready(function() {
+  const tweetData = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac"
+      },
+      content: {
+        text:
+          "If I have seen further it is by standing on the shoulders of giants"
+      },
+      created_at: 1461116232227
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd"
+      },
+      content: {
+        text: "Je pense , donc je suis"
+      },
+      created_at: 1461113959088
+    }
+  ];
 
-// console.log(tweetData.user.handle);
+  const renderTweets = function(tweets) {
+    for (const property of tweets) {
+      const $tweet = createTweetElement(property);
+      $("#tweet-container").append($tweet);
+    }
+  };
 
-const createTweetElement = function(tweetObj) {
-  const markup = `
+  const createTweetElement = function(tweet) {
+    let render = `
   <article class="tweet-container">
           <header class="tweet-header">
             <img
-              src= ${tweetObj.user.avatars}
+              src= ${tweet.user.avatars}
               alt="head"
               width="40"
               height="40"
             />
-            <span class="name">${tweetObj.user.name}</span>
-            <span class="handle">${tweetObj.user.handle}</span>
+            <span class="name">${tweet.user.name}</span>
+            <span class="handle">${tweet.user.handle}</span>
           </header>
           <textarea class="tweet-textarea">
-            ${tweetObj.content.text}
+            ${tweet.content.text}
           </textarea>
           <footer class="tweet-footer">
-            <span class="date-posted">${tweetObj.created_at}</span>
+            <span class="date-posted">${tweet.created_at}</span>
             <span class="twitter-icons">
               <img src="https://img.icons8.com/small/2x/retweet.png" alt="retweet
               width="20" height="20" />
@@ -57,11 +77,9 @@ const createTweetElement = function(tweetObj) {
         </article>
   `;
 
-  return markup;
-};
+    return render;
+  };
 
-const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-// $("#tweets-container").append($tweet);
+renderTweets(tweetData);
+});
